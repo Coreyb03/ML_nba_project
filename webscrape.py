658a99211d768a811331b1378a4d737ff91f9aeb
Corey Becker <thecorey03@gmail.com>
2024-11-22 12:01:26 -0700

    added the ML project to github from colab

---
+++ webscrape.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+# import matplotlib.pyplot as plt
+import numpy as np
+
+url="https://hoopshype.com/salaries/players/"
+
+#scrapes the nba player salaries from hoopshype.com
+dfs = []
+for i in range(1990, 2025):
+        newUrl = url + f"{i}-{i+1}/"
+        r = requests.get(newUrl,timeout=100)
+        r_html = r.text
+
+        soup = BeautifulSoup(r_html, 'html.parser')
+        salary_table = soup.find('table')
+        length=len(salary_table.find_all("td"))
+
+        player_names=[salary_table.find_all("td")[j].text.strip() for j in range(5,length,4)]
+        seas = [i + 1 for _ in range(5,length,4)]
+        sals=[salary_table.find_all("td")[j].text.strip() for j in range(6,length,4)]
+
+        df_dict={'player_names':player_names,
+        'season':seas,
+        'salaries':sals
+        }
+        
+        salary_df=pd.DataFrame(df_dict)
+
+        print(salary_df)
+        dfs.append(salary_df)
+
+
+# print(salary_table.find_all("td"))
+
+totalDF = pd.DataFrame()
+for i in dfs:
+        totalDF = pd.concat([totalDF,i])
+
+print(totalDF)
+# Write to CSV
+try:
+    totalDF.to_csv('./salary2024.csv')
+    print("CSV file created successfully.")
+except Exception as e:
+    print(f"Error writing CSV: {e}")
+
+# column1=[salary_table.find_all("td")[i].text.strip() for i in range(10,length,8)]
+# column2=[salary_table.find_all("td")[i].text.strip() for i in range(11,length,8)]
+# column3=[salary_table.find_all("td")[i].text.strip() for i in range(12,length,8)]
+# column4=[salary_table.find_all("td")[i].text.strip() for i in range(13,length,8)]
+# column5=[salary_table.find_all("td")[i].text.strip() for i in range(14,length,8)]
+# column6=[salary_table.find_all("td")[i].text.strip() for i in range(15,length,8)]
